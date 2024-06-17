ebfc4b6af7d75e21d0285dba68e50292a21f85d4
jun7507 <iam_idea@naver.com>
2024-06-17 15:00:44 +0900

    dags_bash_with_macro_eg2 modify

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -8,11 +8,12 @@ with DAG(
     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    # START_DATE: 2주전 월요일, END_DATE: 2주전 토요일
+    # START_DATE: 2주 전 월요일, END_DATE: 2주 전 토요일
     bash_task_2 = BashOperator(
         task_id='bash_task_2',
-        env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}',
-             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'
+        env={
+            'START_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)).strftime("%Y-%m-%d") }}',
+            'END_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)).strftime("%Y-%m-%d") }}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
